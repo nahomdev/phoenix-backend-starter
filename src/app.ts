1504d9da1c3bdcbbca2b874f31ef486ea63b054e
nahom <nahomdev2021@gmail.com>
2024-03-07 09:29:46 +0300

    initial structure

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,21 @@
 import express from 'express';
+import { useLogger } from './logger';
  
  export default async function createApp(): Promise<Express.Application>{
      const app = express();
-     
 
+     const logger = useLogger();
+     
+     app.get('/test', (req, res) => {
+         
+         logger.info(req.query);   
+         
+         res.send('good!!!');
+     })
+     
+     app.use('*', (req, res) => { 
+         res.send('no url dude');
+     })
+     
      return app;
 }
